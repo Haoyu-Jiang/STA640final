bbcb38478b5f091896d46ae3e5ed0c08a35ccfc4
Jiongran Wang <Enjoywjr@gmail.com>
2022-04-21 11:35:53 -0400

    data generation

---
+++ DataGeneration.R
@@ -0,0 +1,29 @@
+# cohort size
+#n <- c(100, 500, 1000, 2000)
+n <- 100
+cohorts <- 1000
+data_generation <- function(cohorts = 1000, n, SEED = 1){
+  set.seed(seed = SEED)
+  data <- list()
+  for (i in 1:cohorts){
+    Z1 <- Z2 <- rnorm(n, mean = 0, sd = 1)
+    Z3 <- ifelse(runif(n) <= 0.3, 1, 0)
+    # we use true model to generate x
+    beta0 <- 1.5
+    beta1 <- 1
+    beta2 <- -2
+    beta3 <- 1
+    p <- 1 / (1 + exp(-(beta0 + beta1 * Z1 + beta2 * Z2 + beta3 * Z3)))
+    X <- rbinom(n, 1, prob = p)
+    # then generate Y
+    epsilon <- rnorm(n, 0, sd = 0.3)
+    Y <- Z1 + Z3 + 2 * rnorm(n, 0, 1) + epsilon
+    data[[i]] <- data.frame(X = X,
+                            Z1 = Z1,
+                            Z2 = Z2,
+                            Z3 = Z3,
+                            Y = Y)
+  }
+  
+  return (data)
+}
